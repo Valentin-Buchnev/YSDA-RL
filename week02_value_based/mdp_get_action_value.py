187807c5b79c6e000a8575ba96dc337aa14f08e0
VBuchnev <buchnev.valentin@gmail.com>
2020-03-02 12:32:11 +0300

    bug fix

--- week02_value_based/mdp_get_action_value.py
+++ week02_value_based/mdp_get_action_value.py
@@ -2,9 +2,9 @@
 def get_action_value(mdp, state_values, state, action, gamma):
     """ Computes Q(s,a) as in formula above """
 
-    q = 0
+    q = 0.
     
     for next_state in mdp.get_next_states(state, action):
         q += mdp.get_transition_prob(state, action, next_state) * (mdp.get_reward(state, action, next_state) + \
-                                                          gamma * state_values[next_state])
+                                                      gamma * state_values[next_state])              
     return q
